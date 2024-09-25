1c07fdafb2bbb9420eab496b0d9421d2a2a0a9b2
MgMilley12 <159545117+MgMilley12@users.noreply.github.com>
2024-09-25 22:25:06 +0300

    Add files via upload

---
+++ task_11481.py
@@ -0,0 +1,17 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+file = open("17_11481.txt")
+lst = [int(x) for x in file]
+
+lst_osob = [x for x in lst if str(abs(x))[0] == '8']
+max_osob = max(lst_osob)
+
+ans = []
+for i in range(0, len(lst) - 2):
+    x, y, z = lst[i], lst[i + 1], lst[i + 2]
+    if (str(abs(x))[0] == '6') + (str(abs(y))[0] == '6') + (str(abs(z))[0] == '6'):
+        if x + y + z >= max_osob:
+            ans.append(x + y + z)
+    
+print(len(ans), min(ans))
---
+++ task_12255.py
@@ -0,0 +1,8 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+from fnmatch import *
+
+for z in range(98591, 10**12 + 1, 98591):
+    if fnmatch(str(z), '12?3*456??9'):
+        print(z, z // 98591)

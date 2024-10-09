2b0e5eaafc1fe6159980b1d0bd88dead439c1249
MgMilley12 <159545117+MgMilley12@users.noreply.github.com>
2024-10-09 15:33:09 +0300

    Add files via upload

---
+++ 8372.py
@@ -0,0 +1,9 @@
+def F(n):
+    if n < 2:
+        return n
+    if  n >= 2:
+        return F(n//2) * 10 + n % 2
+
+for n in range(1, 10000000):
+    if F(n) == 100000100001000100101:
+        print(n)

5a424bf88cb28097aeba6b402a7bf4d178d99ec7
MgMilley12 <159545117+MgMilley12@users.noreply.github.com>
2024-10-17 08:12:46 +0300

    Update task_9704.py

--- task_9704.py
+++ task_9704.py
@@ -1,16 +1,16 @@
-mus = []
-mus_2 = []
-k = 0
+m_1 = []
+m_2 = []
+c = 0
 f = [int(x) for x in open('17_9704.txt')]
 for i in range(len(f) - 1):
     a = len(str(f[i]))
     b = len(str(f[i + 1]))
     if (a == 2 and b != 2) or (a != 2 and b == 2):
         m = int(f[i]) + int(f[i + 1])
-        mus.append(m)
+        m_1.append(m)
 
 for s in range(len(f)):
-    if f[s] > max(mus):
-        k += 1
-        mus_2.append(f[s])
-print(k, min(mus_2))
+    if f[s] > max(m_1):
+        c += 1
+        m_2.append(f[s])
+print(c, min(m_2))

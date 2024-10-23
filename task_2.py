952953aedab4a0081041e9fadff594d52f31e3be
MgMilley12 <159545117+MgMilley12@users.noreply.github.com>
2024-10-23 22:12:54 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,19 @@
+f = open('24-228_5644.txt').readline()
+s = ''
+l = -1
+for i in range(len(f) - 1):
+    if f[i] == 'X' and f[i + 1] == 'X':
+        if l != -1:
+            f1 = f[l + 2:i]
+            if len(f1) == 11 and f1.isdigit():
+                if f1[0] == '3' and f1[5:7] == '78' and f1[9:] == '45':
+                    s = max(s, f1)
+        l = i
+n = 1
+ns = 0
+for z in s:
+    if int(z) % 2 == 1:
+        n += int(z)
+    else:
+        ns += int(z)
+print(ns + n)
